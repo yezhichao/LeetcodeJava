d68be57b7aef6f347b2ecefede1f82580019e131
yezhichao <yezhichao@dafy.com>
2018-06-06 12:35:08 +0800

    day10, finish 014

---
+++ 014-Longest Common Prefix/Solution.java
@@ -0,0 +1,42 @@
+package com.yzc;
+
+// see detail
+class Solution {
+    public static String longestCommonPrefix(String[] strs) {
+        if (strs.length == 0) {
+            return "";
+        }
+        String prefix = strs[0];
+        for (int i = 1; i < strs.length; i++) {
+            while (strs[i].indexOf(prefix) != 0) {
+                prefix = prefix.substring(0, prefix.length() - 1);
+                if (prefix.isEmpty()) {
+                    return "";
+                }
+            }
+        }
+        return prefix;
+    }
+}
+// class Solution {
+//     public static String longestCommonPrefix(String[] strs) {
+//         int sLen = strs.length;
+//         if (sLen == 0) return "";
+//         int minLen = strs[0].length();
+//         for (int i = 0; i < sLen; i++) {
+//             if (strs[i].length() < minLen) {
+//                 minLen = strs[i].length();
+//             }
+//         }
+
+//         for (int i = 0; i < minLen; i++) {
+//             for (int j = 1; j < sLen; j++) {
+//                 if (strs[j].charAt(i) != strs[0].charAt(i)) {
+//                     return strs[0].substring(0, i);
+//                 }
+//             }
+//         }
+
+//         return strs[0].substring(0, minLen);
+//     }
+// }
