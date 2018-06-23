cfd4a46abf446c5ec72c4fd3beeef3d14075a6a8
yezhichao <yezhichao@dafy.com>
2018-06-23 18:22:11 +0800

    day25, finish 206 for interview reason

---
+++ 206-Reverse Linked List/Solution.java
@@ -0,0 +1,16 @@
+package com.yzc;
+
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        if (head == null) return null;
+        ListNode pre = null;
+        ListNode next = null;
+        while (head != null) {
+            next = head.next;
+            head.next = pre;
+            pre = head;
+            head = next;
+        }
+        return pre;
+    }
+}
